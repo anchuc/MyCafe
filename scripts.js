4a0005f5b23e01d9aa9dadba837b0dc574f73fca
An Nguyễn Trường <90670565+anyeuchuc@users.noreply.github.com>
2021-10-06 14:20:11 +0700

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,19 @@
+        // Get the modal
+        var modal = document.getElementById('menu');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == w3 - modal) {
+                modal.style.display = "none";
+            }
+        }
+
+        var modal = document.getElementById('contact');
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == w3 - modal) {
+                modal.style.display = "none";
+            }
+
+        }
